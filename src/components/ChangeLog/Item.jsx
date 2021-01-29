9fee8bce80c9e7ec71eebeec921c1c44d7f2ca92
Jack Palfrey <jackgpalfrey@outlook.com>
2021-01-29 09:41:53 +0000

    Added pre-changelog updates to changelog

--- src/components/ChangeLog/Item.jsx
+++ src/components/ChangeLog/Item.jsx
@@ -1,20 +1,51 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './style.css'
+
+
+const CHANGES_COLORS = {
+    // Finished Changes
+    "ADDED": "#1b9429",
+    "CHANGED": "#2b6bba",
+    "DEPRECATED": "#a6602b",
+    "REMOVED": "#a62b2b",
+    "FIXED": "#218f7c",
+    // Planned Changes
+    "ADD": "#1b9429",
+    "CHANGE": "#2b6bba",
+    "DEPRECATE": "#a6602b",
+    "REMOVE": "#a62b2b",
+    "FIX": "#218f7c"
+}
 
 function Item(props){
     const [isOpen, setIsOpen] = useState(false)
 
+    useEffect(() => {
+        setIsOpen(props.isOpen)
+    }, [props.isOpen])
+    
+
+
+    let changes = false
+
+    if (props.changes){
+        changes = props.changes.map((value, idx) => {
+            return (<li style={{color: `${CHANGES_COLORS[value[0]]}`}}>{value[0]} - {value[1]}</li>)
+        })
+    }
 
     let ItemInfo = (
         <div className='itemInfoContainer'>
             {props.children}
+            {changes ? <ul className='changesList'>{changes}</ul> : null}
+            <p className='date'>{props.date || 'Unknown'}</p>
         </div>
     )
 
     return (
-        <div className='fullItemContainer'>
-            <div className='TitleContainer' onClick={() => setIsOpen(!isOpen)}>
-                <p>{props.version}</p>
+        <div onClick={() => setIsOpen(!isOpen)} className='fullItemContainer'>
+            <div className='TitleContainer' >
+                <p className='version'>{props.version}</p>
                 <p>{props.title}</p>
             </div>
             {isOpen ? ItemInfo : null}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import Container from '../components/ChangeLog/Container'
 import Item from '../components/ChangeLog/Item'
@@ -6,12 +6,20 @@ import Item from '../components/ChangeLog/Item'
 import changeLogItems from '../data/changelogData.json'
 
 
-let items = changeLogItems.map((value, idx) => {
-    return <Item key={idx} version={value.version} title={value.title}> <p>{value.info}</p> </Item>
-})
 function HomePage(){
+    const [isAllOpen, setIsAllOpen] = useState(false)
+    console.log(isAllOpen)
+    
+    let items = changeLogItems.map((value, idx) => {
+        let dontDisplay = false
+        dontDisplay = value.dontDisplay
+        if (!dontDisplay) return <Item key={idx}  isOpen={isAllOpen} changes={value.modifications} date={value.isNext ? `Planned for ${value.date}` : value.date} version={value.isNext ? 'Next update' : value.version} title={value.title}> <p>{value.info}</p> </Item>
+    
+    })
+
     return (
         <div className='changeLog'>
+            <div className='openAllChangesButton' onClick={() => setIsAllOpen(!isAllOpen)}>{isAllOpen ? 'Close All' : 'Open All'}</div>
             <Container titleBgColor='' titleTxtColor='' infoBgColor='' infoTxtColor=''>
                 {items}
             </Container>

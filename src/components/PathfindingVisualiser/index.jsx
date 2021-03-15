aa9ee9f561d478a1e05e3672cf8d13edad3e7ebb
Jack Palfrey <jackgpalfrey@outlook.com>
2021-03-15 12:24:22 +0000

    Added Basic Grid and Ability to click on / drag over node to change color

--- src/components/PathfindingVisualiser/index.jsx
+++ src/components/PathfindingVisualiser/index.jsx
@@ -1,18 +1,90 @@
-import React from 'react'
-import './style.css'
-import Console from '../Console'
+import React, { useEffect, useState } from 'react';
+import './style.css';
+import Console from '../Console';
 
+function PathfindingVisualiser() {
+	const [grid, setGrid] = useState();
+	const [isMouseDown, setIsMouseDown] = useState(false);
 
-function PathfindingVisualiser(){
-    function AnimateEngine(){
-        return 
-    }
+	useEffect(() => {
+		createGrid();
+	}, []);
 
-    return (
-        <div className='grid-container'>
-            <Console display={true} docsKey='AnimateEngineGrid-clearLoop' AnimateEngine={AnimateEngine}/>
-        </div>
-    )
+	function AnimateEngine() {
+		return;
+	}
+
+	function createGrid() {
+		const NUM_OF_BARS_Y = 20;
+		const NUM_OF_BARS_X = 40;
+		let yAxis = [];
+		for (let yPos = 0; yPos < NUM_OF_BARS_Y; yPos++) {
+			let xAxis = [];
+			for (let xPos = 0; xPos < NUM_OF_BARS_X; xPos++) {
+				xAxis.push({
+					x: xPos,
+					y: yPos,
+					color: 'transparent',
+				});
+			}
+			yAxis.push(xAxis);
+		}
+		setGrid(yAxis);
+	}
+
+	function renderGrid(array) {
+		console.log(array);
+		if (!array) return;
+		console.log('PASS');
+		let yDivs = array.map((xAxis) => {
+			let xDivs = xAxis.map((val) => {
+				return (
+					<div
+						onMouseEnter={() => {
+							if (!isMouseDown) return;
+							setGrid((prevState) => {
+								let prevGrid = prevState.slice();
+								prevGrid[val.y][val.x].color = 'blue';
+								return prevGrid;
+							});
+						}}
+						onMouseDown={() => {
+							setGrid((prevState) => {
+								let prevGrid = prevState.slice();
+								prevGrid[val.y][val.x].color = 'blue';
+								return prevGrid;
+							});
+						}}
+						className='grid-xAxis-Divs'
+						style={{ backgroundColor: val.color }}
+					></div>
+				);
+			});
+			return <div className='grid-yAxis-Divs'>{xDivs}</div>;
+		});
+		return <div>{yDivs}</div>;
+	}
+
+	return (
+		<div
+			className='grid-container'
+			onMouseDown={() => {
+				setIsMouseDown(true);
+			}}
+			onMouseUp={() => {
+				setIsMouseDown(false);
+			}}
+		>
+			<div className='grid-gridContainer'>
+				<div className='grid-gridInnerContainer'>{renderGrid(grid)}</div>
+			</div>
+			<Console
+				display={false}
+				docsKey='AnimateEngineGrid-clearLoop'
+				AnimateEngine={AnimateEngine}
+			/>
+		</div>
+	);
 }
 
-export default PathfindingVisualiser
+export default PathfindingVisualiser;

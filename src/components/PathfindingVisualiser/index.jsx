1e89b371ff5ca012e27ab4fbe28096638070715f
Jack Palfrey <jackgpalfrey@outlook.com>
2021-03-15 13:20:06 +0000

    Added Console

--- src/components/PathfindingVisualiser/index.jsx
+++ src/components/PathfindingVisualiser/index.jsx
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from 'react';
 import './style.css';
 import Console from '../Console';
 
+import getLocaleText from '../../util/getLocaleText';
+const consoleText = getLocaleText('general').console;
+const text = getLocaleText('general').bars;
+const algoData = getLocaleText('algorithmInfo');
+
 function PathfindingVisualiser() {
 	const [grid, setGrid] = useState();
-	const [isMouseDown, setIsMouseDown] = useState(false);
+	const [mouseDown, setMouseDown] = useState(0);
 	const [penColor, setPenColor] = useState('red');
 	const [penType, setPenType] = useState('wall');
+	const [isTerminalOpen, setIsTerminalOpen] = useState(true);
 
 	useEffect(() => {
 		createGrid();
@@ -16,11 +22,20 @@ function PathfindingVisualiser() {
 		return;
 	}
 
-	function handleMouseDown(x, y) {
+	function handleMouseDown(x, y, type) {
 		setGrid((prevState) => {
+			let toType = penType;
+			let toColor = penColor;
+
+			if (mouseDown === 2 || type === 'eraser') {
+				console.log('RIGHT');
+				toType = 'none';
+				toColor = 'transparent';
+			}
+
 			let prevGrid = prevState.slice();
-			prevGrid[y][x].color = penColor;
-			prevGrid[y][x].type = penType;
+			prevGrid[y][x].color = toColor;
+			prevGrid[y][x].type = toType;
 			return prevGrid;
 		});
 	}
@@ -45,18 +60,18 @@ function PathfindingVisualiser() {
 	}
 
 	function renderGrid(array) {
-		console.log(array);
 		if (!array) return;
-		console.log('PASS');
 		let yDivs = array.map((xAxis) => {
 			let xDivs = xAxis.map((val) => {
 				return (
 					<div
 						onMouseEnter={() => {
-							if (!isMouseDown) return;
+							if (mouseDown < 1) return;
 							handleMouseDown(val.x, val.y);
 						}}
-						onMouseDown={() => {
+						onMouseDown={(e) => {
+							if (e.button === 2)
+								return handleMouseDown(val.x, val.y, 'eraser');
 							handleMouseDown(val.x, val.y);
 						}}
 						className={`grid-xAxis-Divs node-type-${val.type}`}
@@ -72,18 +87,34 @@ function PathfindingVisualiser() {
 	return (
 		<div
 			className='grid-container'
-			onMouseDown={() => {
-				setIsMouseDown(true);
+			onMouseDown={(e) => {
+				console.log(e.button);
+				if (e.button === 0) {
+					setMouseDown(1);
+				} else {
+					setMouseDown(2);
+				}
 			}}
 			onMouseUp={() => {
-				setIsMouseDown(false);
+				setMouseDown(0);
 			}}
 		>
 			<div className='grid-gridContainer'>
 				<div className='grid-gridInnerContainer'>{renderGrid(grid)}</div>
 			</div>
+			<nav>
+				<i
+					className='material-icons consoleButton clickable dark'
+					title={
+						isTerminalOpen ? consoleText.closeConsole : consoleText.openConsole
+					}
+					onClick={() => setIsTerminalOpen(!isTerminalOpen)}
+				>
+					{isTerminalOpen ? 'code_off' : 'code'}
+				</i>
+			</nav>
 			<Console
-				display={false}
+				display={isTerminalOpen}
 				docsKey='AnimateEngineGrid-clearLoop'
 				AnimateEngine={AnimateEngine}
 			/>
